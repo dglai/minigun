f10400319fc40b26ef73bdce1d4b9cd4cb3eaa80
Ubuntu <ubuntu@ip-172-31-85-241.ec2.internal>
2019-04-16 20:50:48 +0000

    scripts for different benchmarks

---
+++ scripts/gen_benchmark_graphs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# The script should be called in the root directory of the repo.
+
+ER_N=( 1000 5000 10000 )
+ER_P=( 0.01 0.005 0.001 )
+BA_N=( 1000 5000 10000 )
+BA_M=( 5 10 20 )
+
+mkdir -p bench-graphs
+
+# generate graphs
+echo "Citation graphs..."
+python3 scripts/gen_dgl_graph.py -o bench-graphs/cora.grh --dataset=cora
+python3 scripts/gen_dgl_graph.py -o bench-graphs/citeseer.grh --dataset=citeseer
+python3 scripts/gen_dgl_graph.py -o bench-graphs/pubmed.grh --dataset=pubmed
+
+# generate E-R graphs
+echo "E-R graphs..."
+count=0
+for n in "${ER_N[@]}"
+do
+  for p in "${ER_P[@]}"
+  do
+    echo "N=$n P=$p"
+    python3 scripts/gen_rand_graph.py er -o bench-graphs/er_${count}.grh --er-n=$n --er-p=$p
+    count=$(( $count + 1 ))
+  done
+done
+
+# generate B-A graphs
+echo "B-A graphs..."
+count=0
+for n in "${BA_N[@]}"
+do
+  for m in "${BA_M[@]}"
+  do
+    echo "N=$n M=$m"
+    python3 scripts/gen_rand_graph.py ba -o bench-graphs/ba_${count}.grh --ba-n=$n --ba-m=$m
+    count=$(( $count + 1 ))
+  done
+done
---
+++ scripts/run_benchmarks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# The script should be called in the root directory of the repo.
+# Assume the binary has been built in `./build`
+
+if [ ! -d "bench-graphs" ]; then
+  echo "Please generate benchmark graphs first by scripts/gen_benchmark_graphs.sh"
+  exit 1
+fi
+
+if [ ! -d "build" ]; then
+  echo "Please build the samples first to 'build' folder"
+  exit 1
+fi
+
+# masked mm test
+echo "===========Masked MM test==========="
+for grh in bench-graphs/*;do
+  for D in 8 32 64 128; do
+    for H in 8 16 32; do
+      ./build/samples/benchmark/bench_masked_mm ${grh} $D $H || exit 1
+    done
+  done
+done
+
+# edge softmax test
+echo "===========Edge softmax test==========="
+for grh in bench-graphs/*;do
+  for H in 8 16 32; do
+    ./build/samples/benchmark/bench_edge_softmax ${grh} $H || exit 1
+  done
+done
+
+# spmm test
+echo "===========SPMM test==========="
+for grh in bench-graphs/*;do
+  for D in 8 32 64 128; do
+    for H in 8 16 32; do
+      ./build/samples/benchmark/bench_spmm ${grh} $D $H || exit 1
+    done
+  done
+done
