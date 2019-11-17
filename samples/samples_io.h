303fb0702ad7b0380870077560c1c7d58fbbfa8a
yzh119 <expye@outlook.com>
2019-11-17 18:41:04 +0800

    fix

--- samples/samples_io.h
+++ samples/samples_io.h
@@ -192,14 +192,14 @@ __inline__ SampleCsr ToSampleCsr(const minigun::IntCsr& mg_csr, DLDeviceType dev
 #ifdef __CUDACC__
     const int32_t n_v = mg_csr.row_offsets.length - 1;
     const int32_t n_e = mg_csr.column_indices.length;
-    int32_t* row_offsets = new int32_t[nv + 1];
-    int32_t* column_indices = new int32_t[ne];
-    CUDA_CALL(cudaMemcpy(row_offsets, csr.row_offsets.data, (nv + 1) * sizeof(int32_t),
+    int32_t* row_offsets = new int32_t[n_v + 1];
+    int32_t* column_indices = new int32_t[n_e];
+    CUDA_CALL(cudaMemcpy(row_offsets, csr.row_offsets.data, (n_v + 1) * sizeof(int32_t),
         cudaMemcpyDeviceToHost));
-    CUDA_CALL(cudaMemcpy(column_indices, csr.column_indices.data, ne * sizeof(int32_t),
+    CUDA_CALL(cudaMemcpy(column_indices, csr.column_indices.data, n_e * sizeof(int32_t),
         cudaMemcpyDeviceToHost));
-    std::copy(row_offsets, row_offsets + nv + 1, csr.row_offsets.begin());
-    std::copy(column_indices, column_indices + ne, csr.column_indices.begin());
+    std::copy(row_offsets, row_offsets + n_v + 1, csr.row_offsets.begin());
+    std::copy(column_indices, column_indices + n_e, csr.column_indices.begin());
 #endif  // __CUDACC__
   } else {
     LOG(INFO) << "Unsupported device: " << device;
